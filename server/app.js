00aa15d9d8e332811b2e9980b0b80acef9ac4a9d
Chris Torres <hwct83@gmail.com>
2019-12-29 14:51:43 -0800

    created routes for users

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/my-blog', { useNewUrlParser: true } );
 mongoose.Promise = Promise;
 
 const app = express();
@@ -12,5 +12,7 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.status(200).send();
 });
+
+app.use('/api/users', require('./routes/users'));
 
 module.exports = app;
